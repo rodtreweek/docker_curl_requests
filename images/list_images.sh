3fc7c9f558ea0108b4be78a26b5afd6ec3c7e871
Rod Treweek <rodtreweek@users.noreply.github.com>
2017-10-10 17:22:54 -0700

    Update list_images.sh

--- images/list_images.sh
+++ images/list_images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # List all images.
 curl --unix-socket /var/run/docker.sock http:/v1.24/images/json | python -m json.tool
+# You can also use:
+# echo -e "GET /images/json HTTP/1.0\r\n" | nc -U /var/run/docker.sock
