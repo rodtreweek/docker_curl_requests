ab62f60a421d899e60a35eb187273de9fd8af0d5
Rod Treweek <rodtreweek@users.noreply.github.com>
2017-10-10 18:39:16 -0700

    Update create_run_log.sh

--- create/create_run_log.sh
+++ create/create_run_log.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Create, start, then log a container instance.
 # This must be run directly on the host.
-# Create the container from an image:
+# First, pull the image you wish to install:
+curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X POST http:/v1.24/images/create\?fromImage\
+=<image name, i.e. fluent/fluentd>                                                                                                                                                         
+# Create the container from the image:
 curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d \
 '{"Image": "<image name>", "Cmd": ["echo", "hello world"]}' -X \
 POST http:/v1.24/containers/create
